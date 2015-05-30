3361ae7a7514e8b01e9587cf0a31446b8203ea42
ursopanda <gagallo7@gmail.com>
2015-05-30 16:14:30 +1000

    Using a deque to speedup the LOOCV

--- testingMethods.py
+++ testingMethods.py
@@ -2,19 +2,31 @@ import arff
 
 import knn_wnn
 
+from collections import deque
+
 instances = []
 
 for row in arff.load('ionosphere.arff'):
     instances.append(row);
 
+# Leave-one-out-cross-validation
 def loocv_classes(instances, k):
     correctedPredicted = 0
+
+    # Too expensive to copy the whole list
+    newInstances = deque(instances)
+
     for index in range(instances.__len__()):
-        # Too expensive to copy the whole list
-        newInstances = list(instances)
-        newInstances.pop(index)
-        if instances[index].Class == knn_wnn.classPrediction(newInstances, instances[index], k):
+
+        # Getting the new training set without
+        # one instance 
+        testInstance = newInstances.popleft() # first instance coming out of list
+
+        if testInstance.Class == knn_wnn.classPrediction(newInstances, testInstance, k):
             correctedPredicted += 1
+
+        newInstances.append ( testInstance ) # same instance comes back as the last element
+
     print "Accuracy with LOOCV validation: "
     print correctedPredicted/float(instances.__len__())
 
