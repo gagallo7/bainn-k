2cc8cacdd5631008f836c5c686013e4e47c9c11c
ursopanda <gagallo7@gmail.com>
2015-05-30 23:11:55 +1000

    Refactored the code and added the numerical prediction class

--- knn_wnn.py
+++ knn_wnn.py
@@ -5,16 +5,19 @@
 # Based on
 # http://machinelearningmastery.com/tutorial-to-implement-k-nearest-neighbors-in-python-from-scratch/
 
+# Using the external module arff 0.9
+# URL: https://pypi.python.org/pypi/arff/0.9
 
+import arff
 import math
 import heapq
 from collections import Counter
 
-
+# Return the most common class from the k-nearest neighbours
 def defineClass_kNN(neighbours):
     return Counter(elem[1] for elem in neighbours).most_common()[0][0];
 
-
+# Defines who will be the neighbours for an instance
 def getNeighbours(trainingSet, instance, k):
     dist = []
     length = len(instance) - 1
@@ -26,24 +29,111 @@ def getNeighbours(trainingSet, instance, k):
 
     return heapq.nsmallest(k, dist)
 
-
+# Euclidean distance
 def eucDist(i1, i2, length):
     dist = 0
 
     for x in range(length):
-        dist += pow((i1[x] - i2[x]), 2)
+        if type ( i1[x] ) != type ( str() ):
+            dist += (i1[x] - i2[x])**2
+        else:
+            if ( i1[x] != i2[x] ):
+                dist += 1
+
     return math.sqrt(dist)
 
 
-# Main functions
+# Main classes
 
-def numericalPrediction(trainingSet, instance, k):
-    return predictedNumber;
+# Problem is a class that receives data from a Machine Learning
+# problem
+class Problem:
+    def __init__(self, filename):
+        self.instances = []
 
+        for row in arff.load(filename):
+            self.instances.append(row);
 
-def classPrediction(trainingSet, instance, k):
-    predictedClass = defineClass_kNN(getNeighbours(trainingSet, instance, k))
-    return predictedClass;
+    def prediction( self, trainingSet, instance, k):
+        return
+    # Used for cross-validation
+    # Evaluates one test instance against the splitted training data
+    def evaluate ( self, lhs, newInstances, k ):
+        return
+    # Do the final operations to get the correct evaluation
+    def endEvaluation ( self ):
+        return
+    def printError ( self ):
+        return
 
+# Specializes Problem into a Classification problem
+class Classification ( Problem ):
 
+    def __init__(self, filename):
+        self.accuracy = 0
+        self.tp = 0
+        self.fp = 0
+        self.fn = 0
+        self.fp = 0
+        Problem.__init__ ( self, filename )
+        
+    def prediction( self, trainingSet, instance, k):
+        predictedClass = defineClass_kNN(getNeighbours(trainingSet, instance, k))
+        return predictedClass;
+
+    def evaluate ( self, lhs, newInstances, k ):
+        rhs = self.prediction ( newInstances, lhs, k )
+        self.accuracy += lhs.Class == rhs
+
+    def endEvaluation ( self ):
+        self.accuracy = self.accuracy / float ( len ( self.instances ) )
+
+    def printError ( self ):
+        accStr = "Accuracy: {}".format ( self.accuracy )
+        print accStr
+
+# Specializes Problem into a Regression problem
+class NumericalPrediction ( Problem ):
+    def __init__(self, filename):
+        self.meanAbsError = 0
+        self.relativeAbsError = 0
+        self.rootedSquaredError = 0
+        Problem.__init__ ( self, filename )
+        
+        # Used for relative absolute error
+        self.average = sum ([ c.Class for c in self.instances ]) / len ( self.instances )
+
+    # The predicted number is the average of the 
+    # k nearest neighbours
+    def prediction( self, trainingSet, instance, k):
+        predictedNumber = 0
+
+        for n in getNeighbours(trainingSet, instance, k):
+            predictedNumber += n[1]
+        
+        predictedNumber = predictedNumber / float ( k )
+
+        return predictedNumber;
+
+    def evaluate ( self, lhs, newInstances, k ):
+        rhs = self.prediction ( newInstances, lhs, k )
+
+        self.meanAbsError += abs ( lhs.Class - rhs )
+        self.relativeAbsError += abs ( rhs - self.average )
+        self.rootedSquaredError += ( rhs - self.average )**2
+
+    def endEvaluation ( self ):
+        # The relative and mean abs error have the numerator in common
+        self.relativeAbsError = self.meanAbsError / self.relativeAbsError
+        self.meanAbsError = self.meanAbsError / float ( len ( self.instances ) )
+
+        self.rootedSquaredError = math.sqrt ( self.rootedSquaredError / float ( len ( self.instances ) ) )
+
+    def printError ( self ):
+        meanAbsoluteErrorStr = "Mean absolute error: {}".format ( self.meanAbsError )
+        relativeAbsErrorStr = "Relative absolute error: {}".format ( self.relativeAbsError )
+        rootedSquaredErrorStr = "Rooted Squared error: {}".format ( self.rootedSquaredError )
+        print meanAbsoluteErrorStr
+        print relativeAbsErrorStr
+        print rootedSquaredErrorStr
 # --------------------------------
--- testingMethods.py
+++ testingMethods.py
@@ -1,34 +1,43 @@
+# Falcon team
+# Guilherme Alcarde Gallo       z5030891
+# Pedro Lucas Albuquerque       z5046915
+
+# Based on
+# http://machinelearningmastery.com/tutorial-to-implement-k-nearest-neighbors-in-python-from-scratch/
+
+# Using the external module arff 0.9
+# URL: https://pypi.python.org/pypi/arff/0.9
+
 import arff
 
-import knn_wnn
+from knn_wnn import *
 
 from collections import deque
 
-instances = []
-
-for row in arff.load('ionosphere.arff'):
-    instances.append(row);
-
 # Leave-one-out-cross-validation
-def loocv_classes(instances, k):
-    correctedPredicted = 0
+def loocv_classes(problem, k):
 
     # Too expensive to copy the whole list
-    newInstances = deque(instances)
+    newInstances = deque(problem.instances)
 
-    for index in range(instances.__len__()):
+    for index in range(problem.instances.__len__()):
 
-        # Getting the new training set without
-        # one instance 
+        # Getting the new training set without one instance
         testInstance = newInstances.popleft() # first instance coming out of list
 
-        if testInstance.Class == knn_wnn.classPrediction(newInstances, testInstance, k):
-            correctedPredicted += 1
-
+        problem.evaluate ( testInstance, newInstances, k )
         newInstances.append ( testInstance ) # same instance comes back as the last element
 
-    print "Accuracy with LOOCV validation: "
-    print correctedPredicted/float(instances.__len__())
+    problem.endEvaluation()
+    problem.printError()
 
 
-loocv_classes(instances, 5)
+# The given problems
+c = Classification("ionosphere.arff")
+num = NumericalPrediction("autos2.arff")
+
+print "Testing classification data..."
+loocv_classes( c, 5 )
+
+print "\nTesting numerical data..."
+loocv_classes( num, 5 )
